2c5c7b9b2d89580cf06edda63bcf70d5cde8c080
duxxie <gabri26.cost@gmail.com>
2025-05-01 19:13:29 -0300

    first commit

---
+++ src/main/java/com/example/ainda/AindaApplication.java
@@ -0,0 +1,12 @@
+package com.example.ainda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AindaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AindaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/ainda/AindaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ainda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AindaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
