583511511a109910212a477a40171316b20357ee
duxxie <gabri26.cost@gmail.com>
2025-05-02 18:51:41 -0300

    Cria uma entidade no banco de dados apartir do NinjaModel

--- src/main/java/com/example/ainda/NinjaModel.java
+++ src/main/java/com/example/ainda/NinjaModel.java
@@ -1,10 +1,16 @@
 package com.example.ainda;
 
-import jakarta.persistence.Entity;
-
+import jakarta.persistence.*;
+// Transforma em uma entidade no BD
+// JPA
 @Entity
+@Table(name = "tb_cadastro")
 public class NinjaModel {
 
+    // Indica que o dado é um ID
+    // Acrescenta valor da ID automaticamente
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     long id;
     String nome;
     String email;
