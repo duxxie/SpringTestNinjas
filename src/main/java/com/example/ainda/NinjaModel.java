fa632e6fe06e4f280665d148d92606a30b6a422c
duxxie <gabri26.cost@gmail.com>
2025-05-01 21:12:38 -0300

    NinjaModel criado

---
+++ src/main/java/com/example/ainda/NinjaModel.java
@@ -0,0 +1,21 @@
+package com.example.ainda;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class NinjaModel {
+
+    long id;
+    String nome;
+    String email;
+    int idade;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+}
