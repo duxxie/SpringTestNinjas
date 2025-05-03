21618f15843be5ce42395184d57bda1e607a0108
duxxie <gabri26.cost@gmail.com>
2025-05-03 14:59:04 -0300

    Cria uma entidade no banco de dados apartir do NinjaModel

--- src/main/java/com/example/ainda/NinjaModel.java
+++ src/main/java/com/example/ainda/NinjaModel.java
@@ -11,10 +11,10 @@ public class NinjaModel {
     // Acrescenta valor da ID automaticamente
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    long id;
-    String nome;
-    String email;
-    int idade;
+    private long id;
+    private String nome;
+    private String email;
+    private int idade;
 
     public NinjaModel() {
     }
@@ -22,6 +22,38 @@ public class NinjaModel {
     public NinjaModel(String nome, String email, int idade) {
         this.nome = nome;
         this.email = email;
+        this.idade = idade;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
         this.idade = idade;
     }
 }
